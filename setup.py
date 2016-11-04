6661ed380e14d44445f32cdfa908ee1346cc23bc
Barnaby Gray <barnaby@pickle.me.uk>
2016-11-04 23:33:22 +0000

    Fix long_description

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
 setup(
     name="fabric-anaconda",
-    version="0.0.1",
+    version="0.0.2",
     author='Barnaby Gray',
     author_email='barnaby@pickle.me.uk',
     url='http://pypi.python.org/pypi/fabric-anaconda/',
@@ -10,7 +16,7 @@ setup(
         "Some additional functions for working with anaconda environments "
         "in Fabric."
     ),
-    long_description=open('README.md').read(),
+    long_description=long_description,
     packages=find_packages(),
     install_requires=[
         'Fabric'
